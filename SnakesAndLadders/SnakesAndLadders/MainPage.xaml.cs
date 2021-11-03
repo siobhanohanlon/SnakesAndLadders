929eeb0850d8d20fac0e887979eb66dcff4adb6e
Siobhán O'Hanlon <Siobhanhanlon@outlook.com>
2021-11-03 13:03:56 +0000

    Code Example

--- SnakesAndLadders/SnakesAndLadders/MainPage.xaml.cs
+++ SnakesAndLadders/SnakesAndLadders/MainPage.xaml.cs
@@ -10,117 +10,283 @@ namespace SnakesAndLadders
 {
     public partial class MainPage : ContentPage
     {
-        //Constant to check if player on board
         const int GAME_START_ROW = 11;
-        const int BOARD_RHS = 10, BOARD_LHS = 1;
-        const int BOARD_TOP = 1, BOARD_BOTTOM = 10;
+        const int RHS_COL = 10;
+        const int LHS_COL = 1;
+        const int TOP_ROW = 1;
+        const int BOTTOM_ROW = 10;
+        const int NUMBER_OF_SNAKES = 3;
+        const int NUMBER_OF_LADDERS = 3;
+        const int MAX_PLAYERS = 5;
+        const string ROLL_DICE = "Roll Dice";
+        const string NEW_GAME = "New Game";
+        const string PLAYER = "Player";
 
-        //Create instance of random
+        int _currentPlayer = 1;
+        /*
+         * nice if the boxviews were named Player1, Player2 etc
+         * instead of PurplePiece
+         * name of current is "Player" + _currentPlayer.ToString()
+         */
+        bool _IsLR;
+        int _LR;
+        int _currentBoundary;
         Random random;
+
+        int[][] _snakes;
 
         public MainPage()
         {
             InitializeComponent();
+            // set up the snakes and ladders at the start of app
+            SetUpSnakes();
         }
 
-        private void BtnRollDice_Clicked(object sender, EventArgs e)
+        #region Initialise the board, snakes, ladders
+        /// <summary>
+        /// Reset the Board for New Game. Repositions the pieces on the board, set the direction of movement.
+        /// </summary>
+        private void SetUpNewGame()
         {
-            //Variables
-            int diceRoll = 0;
-            
-            //Generate a random number between 1 and 6
-            if(random == null)
-            {
-                random = new Random();
-            }
-
-            //Set label to dice number
-            diceRoll = random.Next(1, 7);
-            LblDiceRoll.Text = diceRoll.ToString();
-
-            //Move piece, currently only purple
-            MovePiece(diceRoll);
+            //_IsLR = true;   // always starts on Row 10 - moving Left to right
+            _LR = 1;
+            _currentBoundary = RHS_COL;
+            BtnRollDice.Text = ROLL_DICE;
+            _currentPlayer = 1;
+            LblPlayer.Text = PLAYER + " " + _currentBoundary.ToString();
         }
 
-        private void MovePiece(int diceRoll)
+        private void SetUpSnakes()
         {
-            int currentRow = (int)PurplePiece.GetValue(Grid.RowProperty);
-            int currentCol;
-
-            if(currentRow == GAME_START_ROW)
+            // initialise the global arra with all the snakes.
+            // already decided on the representation
+            //int[] OneSnake = new int[4] { 4, 9, 7, 10 };
+            _snakes = new int[3][]
             {
-                //Put piece on board
-                PurplePiece.SetValue(Grid.RowProperty, BOARD_BOTTOM);
-                PurplePiece.SetValue(Grid.ColumnProperty, BOARD_LHS);
+                new int[4] {4, 9, 7, 10},
+                new int[4] {7, 5, 7, 7},
+                new int[4] {2, 4, 2, 9}
+            };  // end of snakes array
+        }
 
-                //Adjust diceroll as first move is adding piece to board
+
+
+        #endregion
+
+        #region All the movement methods for horizontal, vertical, snakes, ladders
+        /// <summary>
+        ///     all the logic for making a move for any piece on theboard.
+        /// </summary>
+        /// <param name="diceRoll">The number of spaces to move.</param>
+        private async void MovePiece(int diceRoll)
+        {
+            BtnRollDice.IsEnabled = false;
+            BtnRollDice.Text = "Moving";
+
+            BoxView currentPiece;
+            string currentPlayerName = PLAYER + _currentPlayer.ToString();
+            currentPiece = (BoxView)GrdGameLayout.FindByName(currentPlayerName);
+            int currentRow = (int)currentPiece.GetValue(Grid.RowProperty);
+            int currentCol, diff;
+
+            if (currentRow == GAME_START_ROW)  // first move only
+            {
+                // put the piece on square 1 on the board
+                currentPiece.SetValue(Grid.RowProperty, BOTTOM_ROW);
+                currentPiece.SetValue(Grid.ColumnProperty, LHS_COL);
                 diceRoll--;
             }
 
-            //Piece is on square 1
-            currentCol = (int)PurplePiece.GetValue(Grid.ColumnProperty);
-            
-            //Wont go off board
-            if((BOARD_RHS - diceRoll) >= currentCol)
-            {
-                //Move right Method
-                MoveRight(PurplePiece, diceRoll);
-                currentCol += diceRoll;
-                PurplePiece.SetValue(Grid.ColumnProperty, currentCol);
-            }
+            ChangeDirection(currentPiece);
+            // piece is on square 1 on the board
+            currentCol = (int)currentPiece.GetValue(Grid.ColumnProperty);
 
-            //Dice roll over left on board
-            //Diceroll > BOARD_RHS - currentcol
+            // 0, 5, 7 -----   (0 - 5) = 5 >= 7
+            //if (Math.Abs(_currentBoundary - diceRoll) >= currentCol)
+            // 0, 7, 5                    0, 3, 5
+            if (Math.Abs(_currentBoundary - currentCol) >= diceRoll)
+            {
+                //MoveRight(PurplePiece, diceRoll);
+                await MoveHorizontal(currentPiece, diceRoll);
+            }
             else
             {
-                //Move right
-                int move = (BOARD_RHS - currentCol);
-                MoveRight(PurplePiece, move);
-                diceRoll -= move;
+                // diceroll < BOARD_RHS - currentCol
+                //MoveRight(PurplePiece, RHS_COL - currentCol); 
+                diff = Math.Abs(_currentBoundary - currentCol);
+                await MoveHorizontal(currentPiece, diff);
 
-                //if greater than 0
-                if(diceRoll>0)
+                diceRoll -= diff;
+                if (diceRoll > 0)
                 {
-                    //Move up a row
-                    MoveUpRow(PurplePiece);
+                    await MoveUpRow(currentPiece);
                     diceRoll--;
-
-                    //Move Left
-                    MoveLeft(PurplePiece, diceRoll);
+                    //MoveLeft(PurplePiece, diceRoll);
+                    await MoveHorizontal(currentPiece, diceRoll);
                 }
+            } // move finished
+            CheckForSnakes(currentPiece);
+            BtnRollDice.Text = ROLL_DICE;
+            BtnRollDice.IsEnabled = true;
+        }   // end of MovePiece()
+
+        /// <summary>
+        /// Move a piece by spaces in either the left or right direction.
+        /// </summary>
+        /// <param name="Piece"></param>
+        /// <param name="Spaces"></param>
+        private async Task MoveHorizontal(BoxView Piece, int Spaces)
+        {
+            int currCol = (int)Piece.GetValue(Grid.ColumnProperty);
+            //int newCol = currCol + Spaces;
+            //if(!_IsLR)
+            //{
+            //    newCol = currCol - Spaces;
+            //}
+            //Piece.SetValue(Grid.ColumnProperty, newCol);
+            // have a value for _LR (1 or -1)
+            //Piece.SetValue(Grid.ColumnProperty, currCol + (Spaces * _LR));
+
+            //Piece.TranslateTo
+            int hDistance = ((int)GrdGameLayout.Width / 12) * Spaces * _LR;
+            uint timeValue = (uint)(Spaces * 150);  // 150ms per space
+            // need the system to wait for the animation to finish
+            // do this using a construct called "await" and "async"
+            await Piece.TranslateTo(hDistance, 0, timeValue);
+            Piece.TranslationX = 0;
+            // also need to change Column Property - translation won't
+            Piece.SetValue(Grid.ColumnProperty, currCol + (Spaces * _LR));
+        }
+
+        private async Task MoveUpRow(BoxView Piece)
+        {
+            int currentRow = (int)Piece.GetValue(Grid.RowProperty);
+            int vDistance = ((int)GrdGameLayout.Height / 12) * -1;
+            // need the system to wait for the animation to finish
+            // do this using a construct called "await" and "async"
+            await Piece.TranslateTo(0, vDistance, 250);
+            Piece.TranslationY = 0;
+            // move up, row value goes down
+            Piece.SetValue(Grid.RowProperty, currentRow - 1);
+            ChangeDirection(Piece);
+        }
+
+        private void MoveLeft(BoxView Piece, int Spaces)
+        {
+            int currCol = (int)Piece.GetValue(Grid.ColumnProperty);
+            Piece.SetValue(Grid.ColumnProperty, currCol - Spaces);
+        }
+
+        /// <summary>
+        /// Change direction of movement for the current piece, reset the boundary for the maths
+        /// </summary>
+        private void ChangeDirection(BoxView Piece)
+        {
+            //_LR *= -1;  // goes from 1 to -1, or -1 to 1
+
+            int currentRow = (int)Piece.GetValue(Grid.RowProperty);
+            if (currentRow % 2 == 0)    // going L to R
+            {
+                //_IsLR = true;
+                _LR = 1;
+                _currentBoundary = RHS_COL;
+            }
+            else
+            {
+                //_IsLR = false;
+                _LR = -1;
+                _currentBoundary = LHS_COL;
             }
         }
 
-        //Move Right Function
-        private void MoveRight(BoxView Piece, int Spaces)
+        private async void MoveAlongSnakeLadderAsync(BoxView Piece,
+                                  int FinishX, int FinishY)
         {
-            //Get current row
-            int currentCol = (int)PurplePiece.GetValue(Grid.ColumnProperty);
+            // 4,9 - 7, 10
+            // need the start point from the piece
+            int startX = (int)Piece.GetValue(Grid.ColumnProperty);
+            int startY = (int)Piece.GetValue(Grid.RowProperty);
+            int hSpaces = FinishX - startX; // 7 - 4 = 3, 5 - 7 = -2
+            int vSpaces = FinishY - startY; // 10 - 9
+            uint timeValue = (uint)(Math.Max(hSpaces, vSpaces) * 150);  // 150ms per space
 
-            //Change collumn movements by dice roll
-            int move= (currentCol + Spaces);
-            Piece.SetValue(Grid.ColumnProperty, move);
+            //Piece.TranslateTo
+            int hDistance = ((int)GrdGameLayout.Width / 12) * hSpaces;
+            int vDistance = ((int)GrdGameLayout.Width / 12) * vSpaces;
+            // need the system to wait for the animation to finish
+            // do this using a construct called "await" and "async"
+            await Piece.TranslateTo(hDistance, vDistance, timeValue);
+            Piece.TranslationX = 0;
+            Piece.TranslationY = 0;
+            // also need to change Column Property - translation won't
+            Piece.SetValue(Grid.ColumnProperty, FinishX);
+            Piece.SetValue(Grid.RowProperty, FinishY);
+            ChangeDirection(Piece); // verifies the direction of movement
         }
 
-        //Move Up Row
-        private void MoveUpRow(BoxView Piece)
-        {
-            //Find current row
-            int currentR = (int)PurplePiece.GetValue(Grid.RowProperty);
 
-            //Increase row by 1, must minus as starting at bottom
-            Piece.SetValue(Grid.RowProperty, currentR);
+        #endregion
+
+        #region Event Handlers and other misc methods
+
+        private void CheckForSnakes(BoxView Piece)
+        {
+            // scroll through the array for snakes and check if the current
+            // piece is on the head of a snake. use a loop, include a break
+            // to exit the loop if I find a snake
+            // OneSnake = new int[4] { HeadC, HeadR, TailC, TailR };
+            int iCounter = 0;
+            int HeadC = 0, HeadR = 1, TailC = 2, TailR = 3;
+            int playerCol = (int)Piece.GetValue(Grid.ColumnProperty);
+            int playerRow = (int)Piece.GetValue(Grid.RowProperty);
+
+            while (iCounter < NUMBER_OF_SNAKES)
+            {
+                if (playerCol == _snakes[iCounter][HeadC] &&
+                    playerRow == _snakes[iCounter][HeadR])  // on a snake
+                {
+                    // could put up message saying "You landed on a snake"
+                    MoveAlongSnakeLadderAsync(Piece,
+                                              _snakes[iCounter][TailC],
+                                              _snakes[iCounter][TailR]);
+                    iCounter = NUMBER_OF_SNAKES + 1;
+                }
+                iCounter++;
+            }   // end while
+
         }
 
-        //Move Left
-        private void MoveLeft(BoxView Piece, int Spaces)
-        {
-            //find current Columnn
-            int currentCol = (int)PurplePiece.GetValue(Grid.ColumnProperty);
 
-            //Change collumn movements by diceroll
-            int move = (currentCol - Spaces);
-            Piece.SetValue(Grid.ColumnProperty, move);
+        private void BtnRollDice_Clicked(object sender, EventArgs e)
+        {
+            int diceRoll = 0;
+            string ButtonText = ((Button)sender).Text;
+
+            //switch (BtnRollDice.Text)
+            switch (ButtonText)
+            {
+                case ROLL_DICE:
+                    {
+                        // generate a random number between 1 and 6
+                        if (random == null)
+                        {
+                            random = new Random();
+                        }
+                        diceRoll = random.Next(1, 7);
+                        LblDiceRoll.Text = diceRoll.ToString(); // update UI
+                        MovePiece(diceRoll);    // currently only move the purple piece
+                        _currentPlayer++;
+                        if (_currentPlayer > MAX_PLAYERS) _currentPlayer = 1;
+                        break;
+                    }
+                case NEW_GAME:
+                    {
+                        SetUpNewGame();
+                        break;
+                    }
+            }
         }
-    }
+
+        #endregion
+    } // end public partial class MainPage : ContentPage
 }
